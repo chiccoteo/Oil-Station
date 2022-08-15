f9497d4345ad60ff0c4677d406a1dc1d2d564937
samir <sharipovsamir480@gmail.com>
2022-08-15 15:40:56 +0500

    bilmillah

---
+++ Oil-Station/src/main/java/com/sigma/oilstation/OilStationApplication.java
@@ -0,0 +1,13 @@
+package com.sigma.oilstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OilStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OilStationApplication.class, args);
+    }
+
+}
---
+++ Oil-Station/src/test/java/com/sigma/oilstation/OilStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigma.oilstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OilStationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
