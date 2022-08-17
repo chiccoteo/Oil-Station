386e10c1b32cf81f6e6d6706fbc7d7c843777df5
Ramziddin <xolmaxmatovramziddin7@gmail.com>
2022-08-17 10:46:27 +0500

    User entity name was changed to "users"

--- Oil-Station/src/main/java/com/sigma/oilstation/entity/User.java
+++ Oil-Station/src/main/java/com/sigma/oilstation/entity/User.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Data
-@Entity
+@Entity(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
