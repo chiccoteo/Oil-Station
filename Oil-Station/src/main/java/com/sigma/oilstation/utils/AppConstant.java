f4b48e1d2f3ca9758deccc38a1ffbf60d3fb10d9
Ramziddin <xolmaxmatovramziddin7@gmail.com>
2022-08-17 16:04:57 +0500

    Default page and size, Max pageSize were determined

--- Oil-Station/src/main/java/com/sigma/oilstation/utils/AppConstant.java
+++ Oil-Station/src/main/java/com/sigma/oilstation/utils/AppConstant.java
@@ -4,4 +4,10 @@ public interface AppConstant {
 
     String BASE_PATH = "/api/oil/station";
 
+    String DEFAULT_PAGE = "0";
+
+    String DEFAULT_SIZE = "10";
+
+    Integer MAX_PAGE_SIZE = 20;
+
 }
