4a3fc93fd79154194ae4da37ff4cff86431767a6
Ramziddin <xolmaxmatovramziddin7@gmail.com>
2022-08-17 10:27:37 +0500

    Address entity was corrected

--- Oil-Station/src/main/java/com/sigma/oilstation/entity/Address.java
+++ Oil-Station/src/main/java/com/sigma/oilstation/entity/Address.java
@@ -1,6 +1,6 @@
 package com.sigma.oilstation.entity;
 
-import com.sigma.oilstation.entity.template.AbsUUID;
+import com.sigma.oilstation.entity.template.AbsLong;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -13,7 +13,7 @@ import javax.persistence.Entity;
 @NoArgsConstructor
 @Entity
 @EqualsAndHashCode(callSuper = true)
-public class Address extends AbsUUID {
+public class Address extends AbsLong {
 
     private String region;
 
