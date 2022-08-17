9bc0094d5a0c19f368cc02da5568bac88025129e
Ramziddin <xolmaxmatovramziddin7@gmail.com>
2022-08-17 10:47:10 +0500

    OutgoingCategory was corrected

--- Oil-Station/src/main/java/com/sigma/oilstation/entity/OutgoingCategory.java
+++ Oil-Station/src/main/java/com/sigma/oilstation/entity/OutgoingCategory.java
@@ -1,7 +1,6 @@
 package com.sigma.oilstation.entity;
 
 import com.sigma.oilstation.entity.template.AbsEntity;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
