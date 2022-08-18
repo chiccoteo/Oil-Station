0d2ce4b298b3d61e68c07484ec5931cb9fe1e73a
Umidjon <suvonqulov17@gmail.com>
2022-08-18 20:35:16 +0500

    my commit

--- Oil-Station/src/main/java/com/sigma/oilstation/controllerImpl/FuelControllerImpl.java
+++ Oil-Station/src/main/java/com/sigma/oilstation/controllerImpl/FuelControllerImpl.java
@@ -2,9 +2,42 @@ package com.sigma.oilstation.controllerImpl;
 
 import com.sigma.oilstation.controller.FuelController;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+import static com.sigma.oilstation.utils.AppConstant.*;
 
 @RestController
 @RequiredArgsConstructor
 public class FuelControllerImpl implements FuelController {
+    @PostMapping(POST_PATH)
+    public HttpEntity<?> addProduct(){
+        return null;
+    }
+
+    @GetMapping(GET_BY_ID_PATH)
+    public HttpEntity<?> getByIdProduct(@PathVariable UUID id){
+        return null;
+    }
+
+    @GetMapping(GET_ALL_PATH)
+    public HttpEntity<?> getAllProduct(@RequestParam(value = "page", defaultValue = DEFAULT_PAGE) Integer page,
+                                       @RequestParam(value = "size", defaultValue = DEFAULT_SIZE) Integer size){
+
+        return null;
+    }
+
+    @PutMapping(PUT_PATH)
+    public HttpEntity<?> updateProduct(@PathVariable UUID id){
+
+        return null;
+    }
+
+    @DeleteMapping(DELETE_PATH)
+    public HttpEntity<?> deleteProduct(@PathVariable UUID id){
+
+        return null;
+    }
 }
--- Oil-Station/src/main/java/com/sigma/oilstation/utils/AppConstant.java
+++ Oil-Station/src/main/java/com/sigma/oilstation/utils/AppConstant.java
@@ -4,6 +4,12 @@ public interface AppConstant {
 
     String BASE_PATH = "/api/oil/station";
 
+    String POST_PATH = "/add";
+    String GET_BY_ID_PATH = "/getById{id}";
+    String GET_ALL_PATH = "/getAll";
+    String PUT_PATH = "/update{id}";
+    String DELETE_PATH = "/delete{id}";
+
     String DEFAULT_PAGE = "0";
 
     String DEFAULT_SIZE = "10";
