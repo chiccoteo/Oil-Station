f2f8db56f0a1b24ee7a6d1e36366d3eebbd50bab
Hasanboy2003 <maxmudovhasanboy070@gmail.com>
2022-08-20 09:47:43 +0500

    prop

--- Oil-Station/src/main/java/com/sigma/oilstation/controller/IncomeFuelController.java
+++ Oil-Station/src/main/java/com/sigma/oilstation/controller/IncomeFuelController.java
@@ -1,6 +1,11 @@
 package com.sigma.oilstation.controller;
 
+import com.sigma.oilstation.entity.IncomeFuel;
+import com.sigma.oilstation.payload.IncomeFuelReqDto;
 import com.sigma.oilstation.utils.AppConstant;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(IncomeFuelController.INCOME_FUEL_CONTROLLER)
@@ -8,4 +13,6 @@ public interface IncomeFuelController {
 
     String INCOME_FUEL_CONTROLLER = AppConstant.BASE_PATH + "/incomeFuel";
 
+    @PostMapping
+    HttpEntity<?> create(@RequestBody IncomeFuelReqDto incomeFuelDto);
 }
