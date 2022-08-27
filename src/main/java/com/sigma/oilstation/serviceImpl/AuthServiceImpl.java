507416af6b39fbc39808c8acf12d11efd817a63f
Ramziddin <xolmaxmatovramziddin7@gmail.com>
2022-08-28 00:09:00 +0500

    Return only token when login

--- src/main/java/com/sigma/oilstation/serviceImpl/AuthServiceImpl.java
+++ src/main/java/com/sigma/oilstation/serviceImpl/AuthServiceImpl.java
@@ -2,7 +2,6 @@ package com.sigma.oilstation.serviceImpl;
 
 import com.sigma.oilstation.entity.User;
 import com.sigma.oilstation.payload.ApiResponse;
-import com.sigma.oilstation.payload.AuthGetDTO;
 import com.sigma.oilstation.payload.LoginDTO;
 import com.sigma.oilstation.secret.JwtProvider;
 import com.sigma.oilstation.service.AuthService;
@@ -25,11 +24,7 @@ public class AuthServiceImpl implements AuthService {
         try {
             Authentication authenticate = manager.authenticate(new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword()));
             User principal = (User) authenticate.getPrincipal();
-            AuthGetDTO authGetDTO = new AuthGetDTO();
-            authGetDTO.setToken(jwtProvider.generateToken(principal));
-            authGetDTO.setRoleType(principal.getRole().getType());
-            authGetDTO.setBlocked(principal.isBlock());
-            return ApiResponse.successResponse("You have successfully logged in", authGetDTO);
+            return ApiResponse.successResponse("You have successfully logged in", jwtProvider.generateToken(principal));
         } catch (Exception e) {
             return ApiResponse.errorResponse("Username or password is incorrect");
         }
