941f9631502a97acfd6d94bd884a2d004c138e40
Hasanboy2003 <maxmudovhasanboy070@gmail.com>
2022-08-30 13:26:34 +0500

    user service

--- src/main/java/com/sigma/oilstation/repository/UserRepository.java
+++ src/main/java/com/sigma/oilstation/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.sigma.oilstation.repository;
 
 import com.sigma.oilstation.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,5 +16,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsernameAndPasswordAndDeletedIsFalse(String username, String password);
 
     boolean existsByUsername(String username);
-
+    List<User> findUserByDeletedFalseOrderByCreatedDateDesc();
+    Page<User> findUsersByDeletedFalse(Pageable pageable);
 }
