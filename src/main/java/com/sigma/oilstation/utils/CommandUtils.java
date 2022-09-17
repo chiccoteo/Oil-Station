28225e69aa36c5109e2701495e223a9aa7bb4f08
Ramziddin <xolmaxmatovramziddin7@gmail.com>
2022-09-17 08:38:14 +0500

    Get method for Notification was corrected

--- src/main/java/com/sigma/oilstation/utils/CommandUtils.java
+++ src/main/java/com/sigma/oilstation/utils/CommandUtils.java
@@ -1,7 +1,6 @@
 package com.sigma.oilstation.utils;
 
 import com.sigma.oilstation.exceptions.PageSizeException;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
